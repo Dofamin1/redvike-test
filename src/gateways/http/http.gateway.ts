e097a0c0b71355b9e638bbb71b53f39cea957d86
akovalenko <akovalenko@danavero.com>
2023-07-11 23:17:34 +0300

    connected swagger

--- src/gateways/http/http.gateway.ts
+++ src/gateways/http/http.gateway.ts
@@ -1,4 +1,6 @@
 import multipart from '@fastify/multipart';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 import config from '../../config';
 import { PROD } from '../../constants/node.evns';
 import { fastify, FastifyInstance } from 'fastify';
@@ -7,7 +9,6 @@ import errorHandler from './http.error.handler';
 import validatorCompiler from './http.validator.compiler';
 import reservationsRouter from '../../components/reservations/transport/http/v1/reservations.router';
 import csvRouter from '../../components/csv/transport/http/v1/csv.router'
-
 
 const server = fastify({
   logger: true,
@@ -25,7 +26,13 @@ async function initHttpGateway(): Promise<FastifyInstance> {
     server.register(reservationsRouter, { prefix: 'v1/reservations' });
     server.register(csvRouter, { prefix: 'v1/csv' });
 
+    server.register(require('@fastify/swagger'), {}); // TODO: add tags
+    server.register(require('@fastify/swagger-ui'), { routePrefix: '/docs' });
+
     await server.listen({ host: config.HOST, port: config.PORT });
+
+    await server.ready();
+    server.swagger();
 
     return server;
   } catch (err) {
