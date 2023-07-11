41ee0490042a162d0a8e160a6f2ed5fec976d349
akovalenko <akovalenko@danavero.com>
2023-07-11 19:08:34 +0300

    added additional data to "prefill db" migration

--- src/modules/postgresDB/migrations/20230711230849_prefill_db.ts
+++ src/modules/postgresDB/migrations/20230711230849_prefill_db.ts
@@ -3,9 +3,13 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex('user').insert({ name: 'Orange' });
 
-  await knex('amenity').insert({ name: 'Gym' });
+  await knex('amenity').insert({ name: 'Swimming pool' });
+  await knex('amenity').insert({ name: 'Tennis cort' });
+
 
   await knex('reservation').insert({ amenity_id: 1, user_id: 1, start_time: 100, end_time: 200, date: 1593648000000 });
+  await knex('reservation').insert({ amenity_id: 2, user_id: 1, start_time: 300, end_time: 400, date: 1593820800000 });
+
 }
 
 export async function down(knex: Knex): Promise<void> {
