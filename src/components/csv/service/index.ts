9c7bbb6a08bbb6a1bb1329f3ee0663f30cff6d5e
akovalenko <akovalenko@danavero.com>
2023-07-11 21:56:54 +0300

    implemented logic of "csv/parse" endpoint

---
+++ src/components/csv/service/index.ts
@@ -0,0 +1,20 @@
+import csv from 'csv-parser';
+import { MultipartFile } from '@fastify/multipart';
+
+export class CSVService {
+
+  async parseCSV(file: MultipartFile): Promise<any[]> {
+    const result: any[] = [];
+
+    await new Promise((res, rej) => {
+      file.file.pipe(csv({ separator: ';' }))
+          .on('data', (data) => result.push(data))
+          .on('end', () => res(result))
+          .on('error', (e: Error) => rej(e));
+    });
+
+    return result;
+  }
+}
+
+export default new CSVService();
